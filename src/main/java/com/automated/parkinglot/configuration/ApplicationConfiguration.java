c33ac72463580dc3ec042be25d49b79528f5a03e
Pradeep Kumar Manimuthu <prade.ycsm@gmail.com>
2022-05-25 17:57:53 +0530

    Enabled CORS

--- src/main/java/com/automated/parkinglot/configuration/ApplicationConfiguration.java
+++ src/main/java/com/automated/parkinglot/configuration/ApplicationConfiguration.java
@@ -3,6 +3,8 @@ package com.automated.parkinglot.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -13,5 +15,15 @@ public class ApplicationConfiguration {
         modelMapper.getConfiguration().setFieldMatchingEnabled(true)
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
         return modelMapper;
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
