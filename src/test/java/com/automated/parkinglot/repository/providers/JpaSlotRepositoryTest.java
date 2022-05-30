147e684a38f8f49d259fe4dfc0952613d9686c05
Pradeep Kumar Manimuthu <prade.ycsm@gmail.com>
2022-05-30 23:01:14 +0530

    Unit tests for slot booking and management

---
+++ src/test/java/com/automated/parkinglot/repository/providers/JpaSlotRepositoryTest.java
@@ -0,0 +1,130 @@
+package com.automated.parkinglot.repository.providers;
+
+import com.automated.parkinglot.models.enums.GenericType;
+import com.automated.parkinglot.models.enums.SlotStatus;
+import com.automated.parkinglot.models.parking.ParkingFloor;
+import com.automated.parkinglot.models.parking.ParkingLot;
+import com.automated.parkinglot.models.parking.Slot;
+import com.automated.parkinglot.repository.ParkingLotRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.stream.StreamSupport;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@Import({JpaSlotRepository.class, JpaParkingFloorRepository.class})
+class JpaSlotRepositoryTest {
+
+  private static final int SLOT_INITIAL_SIZE = 4;
+  private static final int SLOT_VACANT_COUNT = 3;
+  private static final int SLOT_OCCUPIED_COUNT = 1;
+  private static final String SLOT_NAME_TO_FIND = "LOT1_1_2";
+
+  @Autowired private JpaSlotRepository jpaSlotRepository;
+  @Autowired private ParkingLotRepository parkingLotRepository;
+  @Autowired private JpaParkingFloorRepository jpaParkingFloorRepository;
+
+  private ParkingFloor parkingFloor;
+
+  private ParkingFloor generateParkingFloor() {
+    var parkingLot = new ParkingLot();
+    var parkingFloor = ParkingFloor.builder().parkingLot(parkingLot).build();
+    parkingLotRepository.save(parkingLot);
+    jpaParkingFloorRepository.save(parkingFloor);
+    return parkingFloor;
+  }
+
+  private void addSlotsForBasicTest(ParkingFloor parkingFloor) {
+    addSlots(1, GenericType.CAR, SlotStatus.VACANT, "LOT1_1_1", parkingFloor);
+    addSlots(2, GenericType.CAR, SlotStatus.VACANT, "LOT1_1_2", parkingFloor);
+    addSlots(3, GenericType.BIKE, SlotStatus.VACANT, "LOT1_1_3", parkingFloor);
+    addSlots(4, GenericType.BIKE, SlotStatus.OCCUPIED, "LOT1_1_4", parkingFloor);
+  }
+
+  private void addSlots(
+      int slotId,
+      GenericType slotType,
+      SlotStatus slotStatus,
+      String name,
+      ParkingFloor parkingFloor) {
+    jpaSlotRepository.save(
+        Slot.builder()
+            .slotId(slotId)
+            .slotType(slotType)
+            .slotStatus(slotStatus)
+            .name(name)
+            .parkingFloor(parkingFloor)
+            .build());
+  }
+
+  @BeforeEach
+  void setUp() {
+    // given
+    parkingFloor = generateParkingFloor();
+    addSlotsForBasicTest(parkingFloor);
+  }
+
+  @Test
+  void findAll_WhenMultipleSlotsPresent() {
+    // when
+    var slots = jpaSlotRepository.findAllSlotsByParkingFloor(parkingFloor.getParkingFloorId());
+
+    // then
+    assertThat(StreamSupport.stream(slots.spliterator(), false).count())
+        .isEqualTo(SLOT_INITIAL_SIZE);
+  }
+
+  @Test
+  void findAll_WhenNoSlotsPresent() {
+    // given
+    jpaSlotRepository.deleteAll();
+
+    // when
+    var slots = jpaSlotRepository.findAllSlotsByParkingFloor(parkingFloor.getParkingFloorId());
+
+    // then
+    assertThat(StreamSupport.stream(slots.spliterator(), false).count()).isEqualTo(0);
+  }
+
+  @Test
+  void findByName() {
+    // when
+    var optionalSlot = jpaSlotRepository.findByName(SLOT_NAME_TO_FIND);
+
+    // then
+    assertThat(optionalSlot.isEmpty()).isFalse();
+    assertThat(optionalSlot.get().getName()).isEqualTo(SLOT_NAME_TO_FIND);
+  }
+
+  @Test
+  void getAllSlots_ForOccupied() {
+    // when
+    var slots =
+        jpaSlotRepository.getAllSlotsForStatus(
+            SlotStatus.OCCUPIED, parkingFloor.getParkingLot().getParkingLotId());
+    var noOfSlotsReturned = StreamSupport.stream(slots.spliterator(), false).count();
+
+    // then
+    assertThat(noOfSlotsReturned).isEqualTo(SLOT_OCCUPIED_COUNT);
+  }
+
+  @Test
+  void getAllSlots_ForVacant() {
+    // when
+    var slots =
+        jpaSlotRepository.getAllSlotsForStatus(
+            SlotStatus.VACANT, parkingFloor.getParkingLot().getParkingLotId());
+    var noOfSlotsReturned = StreamSupport.stream(slots.spliterator(), false).count();
+
+    // then
+    assertThat(noOfSlotsReturned).isEqualTo(SLOT_VACANT_COUNT);
+  }
+}
---
+++ src/test/java/com/automated/parkinglot/strategies/slot_booking/LowestOnAllFromDbTest.java
@@ -0,0 +1,183 @@
+package com.automated.parkinglot.strategies.slot_booking;
+
+import com.automated.parkinglot.models.enums.GenericType;
+import com.automated.parkinglot.models.enums.SlotStatus;
+import com.automated.parkinglot.models.parking.ParkingFloor;
+import com.automated.parkinglot.models.parking.ParkingLot;
+import com.automated.parkinglot.models.parking.Slot;
+import com.automated.parkinglot.repository.ParkingLotRepository;
+import com.automated.parkinglot.repository.providers.JpaParkingFloorRepository;
+import com.automated.parkinglot.repository.providers.JpaSlotRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@Import({LowestOnAllFromDb.class, JpaParkingFloorRepository.class, JpaSlotRepository.class})
+class LowestOnAllFromDbTest {
+
+  private static final String LOWEST_SLOT_FOR_CAR = "LOT1_5_1";
+  private static final String LOWEST_SLOT_FOR_BIKE = "LOT1_1_4";
+  private static final String LOWEST_SLOT_FOR_TRUCK = "LOT1_14_4";
+
+  private ParkingLot parkingLot;
+  @Autowired private LowestOnAllFromDb lowestOnAllFromDb;
+
+  @Autowired private ParkingLotRepository parkingLotRepository;
+  @Autowired private JpaSlotRepository jpaSlotRepository;
+  @Autowired private JpaParkingFloorRepository jpaParkingFloorRepository;
+
+  private ParkingFloor generateParkingFloor(ParkingLot parkingLot) {
+    var parkingFloor = ParkingFloor.builder().parkingLot(parkingLot).build();
+    parkingLotRepository.save(parkingLot);
+    jpaParkingFloorRepository.save(parkingFloor);
+    return parkingFloor;
+  }
+
+  private void addSlotsForBasicTest(
+      ParkingFloor parkingFloorFirst,
+      ParkingFloor parkingFloorSecond,
+      ParkingFloor parkingFloorThird) {
+    // first floor
+    addSlots(1, GenericType.CAR, SlotStatus.OCCUPIED, "LOT1_1_1", parkingFloorFirst);
+    addSlots(2, GenericType.CAR, SlotStatus.OCCUPIED, "LOT1_1_2", parkingFloorFirst);
+    addSlots(3, GenericType.BIKE, SlotStatus.OCCUPIED, "LOT1_1_3", parkingFloorFirst);
+    addSlots(4, GenericType.BIKE, SlotStatus.VACANT, "LOT1_1_4", parkingFloorFirst);
+
+    // second floor
+    addSlots(5, GenericType.CAR, SlotStatus.VACANT, "LOT1_5_1", parkingFloorSecond);
+    addSlots(6, GenericType.CAR, SlotStatus.VACANT, "LOT1_6_2", parkingFloorSecond);
+    addSlots(7, GenericType.BIKE, SlotStatus.VACANT, "LOT1_7_3", parkingFloorSecond);
+    addSlots(8, GenericType.BIKE, SlotStatus.OCCUPIED, "LOT1_8_4", parkingFloorSecond);
+
+    // third floor
+    addSlots(9, GenericType.CAR, SlotStatus.VACANT, "LOT1_9_1", parkingFloorThird);
+    addSlots(10, GenericType.CAR, SlotStatus.VACANT, "LOT1_10_2", parkingFloorThird);
+    addSlots(11, GenericType.BIKE, SlotStatus.VACANT, "LOT1_11_3", parkingFloorThird);
+    addSlots(12, GenericType.BIKE, SlotStatus.OCCUPIED, "LOT1_12_4", parkingFloorThird);
+    addSlots(13, GenericType.TRUCK, SlotStatus.OCCUPIED, "LOT1_13_4", parkingFloorThird);
+    addSlots(14, GenericType.TRUCK, SlotStatus.VACANT, "LOT1_14_4", parkingFloorThird);
+  }
+
+  private void addSlotsForNoAvailableSlotTest(
+      ParkingFloor parkingFloorFirst, ParkingFloor parkingFloorSecond) {
+    // first floor
+    addSlots(1, GenericType.CAR, SlotStatus.OCCUPIED, "LOT1_1_1", parkingFloorFirst);
+    addSlots(2, GenericType.TRUCK, SlotStatus.OCCUPIED, "LOT1_1_2", parkingFloorFirst);
+    addSlots(3, GenericType.BIKE, SlotStatus.OCCUPIED, "LOT1_1_3", parkingFloorFirst);
+    addSlots(4, GenericType.BIKE, SlotStatus.OCCUPIED, "LOT1_1_4", parkingFloorFirst);
+
+    // second floor
+    addSlots(5, GenericType.CAR, SlotStatus.OCCUPIED, "LOT1_5_1", parkingFloorSecond);
+    addSlots(6, GenericType.CAR, SlotStatus.OCCUPIED, "LOT1_6_2", parkingFloorSecond);
+    addSlots(7, GenericType.TRUCK, SlotStatus.OCCUPIED, "LOT1_7_3", parkingFloorSecond);
+    addSlots(8, GenericType.BIKE, SlotStatus.OCCUPIED, "LOT1_8_4", parkingFloorSecond);
+  }
+
+  private void addSlots(
+      int slotId,
+      GenericType slotType,
+      SlotStatus slotStatus,
+      String name,
+      ParkingFloor parkingFloor) {
+    jpaSlotRepository.save(
+        Slot.builder()
+            .slotId(slotId)
+            .slotType(slotType)
+            .slotStatus(slotStatus)
+            .name(name)
+            .parkingFloor(parkingFloor)
+            .build());
+  }
+
+  @BeforeEach
+  void setUp() {
+    // given
+    var parkingLot = new ParkingLot();
+    var parkingFloorFirst = generateParkingFloor(parkingLot);
+    var parkingFloorSecond = generateParkingFloor(parkingLot);
+    var parkingFloorThird = generateParkingFloor(parkingLot);
+    addSlotsForBasicTest(parkingFloorFirst, parkingFloorSecond, parkingFloorThird);
+    this.parkingLot = parkingFloorFirst.getParkingLot();
+  }
+
+  private void setUpForAllSlotsOccupied() {
+    // given
+    jpaSlotRepository.deleteAll();
+    var parkingLot = new ParkingLot();
+    var parkingFloorFirst = generateParkingFloor(parkingLot);
+    var parkingFloorSecond = generateParkingFloor(parkingLot);
+    addSlotsForNoAvailableSlotTest(parkingFloorFirst, parkingFloorSecond);
+    this.parkingLot = parkingFloorFirst.getParkingLot();
+  }
+
+  @Test
+  void tryBookSlotWhenAvailable_ForCar() {
+    // when
+    var slot = lowestOnAllFromDb.bookSlot(parkingLot.getParkingLotId(), GenericType.CAR);
+
+    // then
+    assertThat(slot.getName()).isEqualTo(LOWEST_SLOT_FOR_CAR);
+  }
+
+  @Test
+  void tryBookSlotWhenAvailable_ForBike() {
+    // when
+    var slot = lowestOnAllFromDb.bookSlot(parkingLot.getParkingLotId(), GenericType.BIKE);
+
+    // then
+    assertThat(slot.getName()).isEqualTo(LOWEST_SLOT_FOR_BIKE);
+  }
+
+  @Test
+  void tryBookSlotWhenAvailable_ForTruck() {
+    // when
+    var slot = lowestOnAllFromDb.bookSlot(parkingLot.getParkingLotId(), GenericType.TRUCK);
+
+    // then
+    assertThat(slot.getName()).isEqualTo(LOWEST_SLOT_FOR_TRUCK);
+  }
+
+  @Test
+  void tryBookSlotWhenNoSlotAvailable_ForCar() {
+    // given
+    setUpForAllSlotsOccupied();
+
+    // when
+    var slot = lowestOnAllFromDb.bookSlot(parkingLot.getParkingLotId(), GenericType.CAR);
+
+    // then
+    assertThat(slot).isNull();
+  }
+
+  @Test
+  void tryBookSlotWhenNoSlotAvailable_ForBike() {
+    // given
+    setUpForAllSlotsOccupied();
+
+    // when
+    var slot = lowestOnAllFromDb.bookSlot(parkingLot.getParkingLotId(), GenericType.BIKE);
+
+    // then
+    assertThat(slot).isNull();
+  }
+
+  @Test
+  void tryBookSlotWhenNoSlotAvailable_ForTruck() {
+    // given
+    setUpForAllSlotsOccupied();
+
+    // when
+    var slot = lowestOnAllFromDb.bookSlot(parkingLot.getParkingLotId(), GenericType.TRUCK);
+
+    // then
+    assertThat(slot).isNull();
+  }
+}
